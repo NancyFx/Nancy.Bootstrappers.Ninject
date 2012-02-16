4b2ec286a0ebc62d685183ed412fb8350790df84
Andreas Hakansson <andreas@selfinflicted.org>
2012-02-16 12:46:00 +0100

    Added support for Func parameters and added tests
    Using a custom Ninject module, the bootstrapper is now able to resolve
    Func<T> dependencies. Also added a test project and a tests that makes
    sure that the INancyEngine can be resolved from the container.

---
+++ src/Nancy.Bootstrappers.Ninject.Tests/FakeNinjectNancyBootstrapper.cs
@@ -0,0 +1,9 @@
+﻿namespace Nancy.Bootstrappers.Ninject.Tests
+{
+    /// <summary>
+    /// Fake Ninject boostrapper that can be used for testing.
+    /// </summary>
+    public class FakeNinjectNancyBootstrapper : NinjectNancyBootstrapper
+    {
+    }
+}
